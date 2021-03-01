a57b226925b7aebcc2ddace15d9c4ac1cdd9e0da
Diego Roman <diegormnv209@gmail.com>
2021-03-01 13:25:40 -0800

    updated migration table

--- data/migrations/20210301105042_first-migration.js
+++ data/migrations/20210301105042_first-migration.js
@@ -6,60 +6,25 @@ exports.up = function(knex) {
       tbl.string('username').notNullable().unique()
       tbl.string('password').notNullable()
   })
-  .createTable('categories', tbl =>{
-      tbl.increments('category_id')
-      tbl.string('category').notNullable()
-  })
-  .createTable('ingredients', tbl =>{
-      tbl.increments('ingredient_id')
-      tbl.string('ingredient').notNullable()
-  })
+
   .createTable('recipes', tbl =>{
       tbl.increments('recipe_id')
-      tbl.string('title').notNullable()
-      tbl.string('source').notNullable()
-      tbl.integer('category_id')
-        .references('category_id')
-        .inTable('categories')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
+      tbl.string('title', 50).notNullable()
+      tbl.string('source', 50).notNullable()
+      tbl.string('category', 50).notNullable()
+      tbl.string('instructions', 2500).notNullable()
+      tbl.string('ingredients', 2500).notNullable()
       tbl.integer('user_id')
         .references('user_id')
         .inTable('users')
         .onUpdate('CASCADE')
-        .onDelete('CASCADE')
+        .onDelete('RESTRICT')
   })
-  .createTable('steps', tbl =>{
-      tbl.increments('steps_id')
-      tbl.string('instructions').notNullable()
-      tbl.integer('recipe_id')
-        .references('recipe_id')
-        .inTable('recipes')
-        .onDelete('Cascade')
-        .onUpdate('Cascade')
-  })
-  .createTable('steps_ingredients', tbl =>{
-      tbl.increments('step_ingredient_id')
-      tbl.integer('ingredient_id')
-        .references('ingredient_id')
-        .inTable('ingredients')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      tbl.string('quantity').notNullable()
-      tbl.integer('steps_id')
-        .references('steps_id')
-        .inTable('steps')
-        .onDelete('CASCADE')
-        .onUpdate('Cascade')
-  })
+  
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('steps_ingredients')
-    .dropTableIfExists('steps')
     .dropTableIfExists('recipes')
-    .dropTableIfExists('ingredients')
-    .dropTableIfExists('categories')
     .dropTableIfExists('users')
 };
